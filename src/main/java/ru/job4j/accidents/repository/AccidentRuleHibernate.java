85a63dda888bd8fdc656beb262f79ae7b84f7e47
rusta <rustam_ussr@mail.ru>
2023-04-04 18:24:08 +0300

    1. Spring ORM [#2093]

--- src/main/java/ru/job4j/accidents/repository/AccidentRuleHibernate.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRuleHibernate.java
@@ -33,47 +33,12 @@ public class AccidentRuleHibernate implements AccidentRepository {
         return accident;
     }
 
-//    @Override
-//    public Accident add(Accident accident) {
-//        System.out.println("!!!!!!!!!!!!!!!!!!"+accident);
-//        try (Session session = sf.openSession()) {
-//            session.save(accident);
-//            return accident;
-//        }
-//    }
-
-//    @Override
-//    public Accident add(Accident accident) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery("""
-//                    INSERT INTO Accident
-//                    (name, text, address, type_id)
-//                    VALUES (:name, :text, :address, :type_id)
-//                    """)
-//                    .setParameter("name", accident.getName())
-//                    .setParameter("text", accident.getText())
-//                    .setParameter("address", accident.getAddress())
-//                    .setParameter("type_id", accident.getType().getId())
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//            accident = session.get(Accident.class, accident.getId());
-//            System.out.println("!!!!!!!!!!!!!!!!!!!!"+accident);
-////            accidentRuleJdbcTemplate.add(accident.getId(), accident.getRules());
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        return accident;
-//    }
-
     @Override
     public boolean replace(int id, Accident accident) {
         boolean result = true;
         Session session = sf.openSession();
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         try {
-//                        accidentRuleJdbcTemplate.replace(id,accident);
             System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
             session.beginTransaction();
             session.createQuery("""
@@ -137,76 +102,4 @@ public class AccidentRuleHibernate implements AccidentRepository {
         session.close();
         return result;
     }
-
-//    @Override
-//    public Task save(Task task) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.save(task);
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        session.close();
-//        return task;
-//    }
-//
-//    @Override
-//    public void deleteById(int id) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery(
-//                            "DELETE Task WHERE id = :fId")
-//                    .setParameter("fId", id)
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        session.close();
-//    }
-//
-//    @Override
-//    public void update(Task task) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery("""
-//                    UPDATE Task
-//                    SET description = :description, created = :created,
-//                        done = :done
-//                    WHERE id = :id
-//                    """)
-//                    .setParameter("description", task.getDescription())
-//                    .setParameter("created", task.getCreated())
-//                    .setParameter("done", task.isDone())
-//                    .setParameter("fId", task.getId())
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//    }
-//
-//    @Override
-//    public Optional<Task> findById(int id) {
-//        Session session = sf.openSession();
-//        session.beginTransaction();
-//        Task result = session.get(Task.class, id);
-//        session.getTransaction().commit();
-//        session.close();
-//        return Optional.ofNullable(result);
-//    }
-//
-//    @Override
-//    public Collection<Task> findAll() {
-//        Session session = sf.openSession();
-//        session.beginTransaction();
-//        List<Task> result = session.createQuery("from Task ORDER BY id", Task.class).list();
-//        session.getTransaction().commit();
-//        session.close();
-//        return result;
-//    }
 }
--- src/main/java/ru/job4j/accidents/repository/RuleHibernate.java
+++ src/main/java/ru/job4j/accidents/repository/RuleHibernate.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 @Primary
 public class RuleHibernate implements RuleRepository {
     private final SessionFactory sf;
-//    private final AccidentRuleJdbcTemplate accidentRuleJdbcTemplate;
 
     @Override
     public Rule add(Rule rule) {
@@ -84,101 +83,4 @@ public class RuleHibernate implements RuleRepository {
         session.close();
         return result;
     }
-
-//    @Override
-//    public Accident add(Accident accident) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery("""
-//                    INSERT INTO Accident
-//                    (name, text, address, type_id)
-//                    VALUES(:name, :text, :address, :type_id)
-//                    """)
-//                    .setParameter("name", accident.getName())
-//                    .setParameter("text", accident.getText())
-//                    .setParameter("address", accident.getAddress())
-//                    .setParameter("type_id", accident.getType().getId())
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//            accident = session.get(Accident.class, accident.getId());
-//            accidentRuleJdbcTemplate.add(accident.getId(), accident.getRules());
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        return accident;
-//    }
-
-
-//    @Override
-//    public Task save(Task task) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.save(task);
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        session.close();
-//        return task;
-//    }
-//
-//    @Override
-//    public void deleteById(int id) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery(
-//                            "DELETE Task WHERE id = :fId")
-//                    .setParameter("fId", id)
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//        session.close();
-//    }
-//
-//    @Override
-//    public void update(Task task) {
-//        Session session = sf.openSession();
-//        try {
-//            session.beginTransaction();
-//            session.createQuery("""
-//                    UPDATE Task
-//                    SET description = :description, created = :created,
-//                        done = :done
-//                    WHERE id = :id
-//                    """)
-//                    .setParameter("description", task.getDescription())
-//                    .setParameter("created", task.getCreated())
-//                    .setParameter("done", task.isDone())
-//                    .setParameter("fId", task.getId())
-//                    .executeUpdate();
-//            session.getTransaction().commit();
-//        } catch (Exception e) {
-//            session.getTransaction().rollback();
-//        }
-//    }
-//
-//    @Override
-//    public Optional<Task> findById(int id) {
-//        Session session = sf.openSession();
-//        session.beginTransaction();
-//        Task result = session.get(Task.class, id);
-//        session.getTransaction().commit();
-//        session.close();
-//        return Optional.ofNullable(result);
-//    }
-//
-//    @Override
-//    public Collection<Task> findAll() {
-//        Session session = sf.openSession();
-//        session.beginTransaction();
-//        List<Task> result = session.createQuery("from Task ORDER BY id", Task.class).list();
-//        session.getTransaction().commit();
-//        session.close();
-//        return result;
-//    }
 }
